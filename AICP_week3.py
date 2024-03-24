5d6a2032b8792500c6fc06adbd45b9aac3539c99
Hafsa Javed <hafsajavedpak@outlook.com>
2024-03-24 10:11:31 +0500

    a few minoe chnages

--- AICP_week3.py
+++ AICP_week3.py
@@ -1,41 +1,60 @@
 from statistics import mean
 
 class Cow:
-    milking_time = 0
+    days = {
+        1: "Monday - 1st milking",
+        2: "Monday - 2nd milking",
+        3: "Tuesday - 1st milking",
+        4: "Tuesday - 2nd milking",
+        5: "Wednesday - 1st milking",
+        6: "Wednesday - 2nd milking",
+        7: "Thursday - 1st milking",
+        8: "Thursday - 2nd milking",
+        9: "Friday - 1st milking",
+        10: "Friday - 2nd milking",
+        11: "Saturday - 1st milking",
+        12: "Saturday - 2nd milking",
+        13: "Sunday - 1st milking",
+        14: "Sunday - 2nd milking",
+    }
+    
+    milking_time = 1  # Start from 1
     yield_weekly = 0
     yield_average = 0
-    def __init__ (self, cow_id) :
-        try :
-            if not 100 <= cow_id <= 999 :
+    
+    def __init__(self, cow_id):
+        try:
+            if not 100 <= cow_id <= 999:
                 raise ValueError("Cow ID must be between 100 and 999.")
-            else: 
+            else:
                 self.cow_id = cow_id
         except ValueError as e:
             print(e)
 
         self.yield_total = [0.0] * 14
-    
+
     def milk_cow(self):
-        self.milking_time += 1
         # week is finished
-        if self.milking_time > 7 :
-            self.milking_time = 0
+        if self.milking_time > 14:
+            self.milking_time = 1  # Reset to 1 for next week
             self.yield_average = mean(self.yield_total)
             self.yield_weekly = sum(self.yield_total)
-        yield_amount = round(float(input(f"Enter the volume of milk milked from cow {self.cow_id}:\n")), 1)
-        self.yield_total[int(self.milking_time/2)] = yield_amount
+        yield_amount = round(float(input(f"Enter the volume of milk milked from cow {self.cow_id} for {self.days[self.milking_time]}:\n")), 1)
+        self.yield_total[self.milking_time - 1] = yield_amount
+        self.milking_time += 1
 
     def calculate_cow_stuff(self):
-        if self.milking_time > 7 :  
-            self.milking_time = 0
+        if self.milking_time > 14:
+            self.milking_time = 1
             self.yield_average = mean(self.yield_total)
-            self.yield_weekly = sum(self.yield_total)           
+            self.yield_weekly = sum(self.yield_total)
         else:
             pass
 
 class Herd:
     herd = {}
     num_cows = 0
+
     def __init__(self, num_cows):
         self.num_cows = num_cows
         self.create_cows()
@@ -66,8 +85,7 @@ class Herd:
         for cow_id in low_volume_cows:
             print(f"Cow ID: {cow_id}")
 
-# Main Program
-if __name__ == "__main__":
+def main() -> None:
     num_cows = int(input("Enter the number of cows in the herd: "))
     herd = Herd(num_cows)
 
@@ -79,3 +97,6 @@ if __name__ == "__main__":
 
     print("\nTask 3: Identify the Most Productive Cow and Low-Volume Producers")
     herd.identify_cows()
+
+if __name__ == "__main__":
+    main()
